db16727c05874fdb1c675a1ec5ffa6ff133af135
Thibault_Azemar <thibault.azemar@gmail.com>
2023-02-11 18:13:41 +0100

    Add model, data and controller

---
+++ Backend/src/main/java/com/projetkfet/backend/data/UserRepository.java
@@ -0,0 +1,8 @@
+package com.projetkfet.backend.data;
+
+import com.projetkfet.backend.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
