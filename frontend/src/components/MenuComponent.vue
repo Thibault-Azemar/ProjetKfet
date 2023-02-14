7db3eb5b6a18cfd396928d9dfae83755c30e2d9b
Aurélie THURIOT <aurelie.thuriot@etu.univ-tours.fr>
2023-02-14 15:16:19 +0100

    Menu v1 terminé + responsive

--- frontend/src/components/MenuComponent.vue
+++ frontend/src/components/MenuComponent.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="menu">
-    <router-link to="/" class="">Home</router-link>
+    <div id="items">
+      <router-link to="/test" class=""><img src="../assets/pictures/pen-to-square.svg"></router-link>
+      <router-link to="/t" class=""><img src="../assets/pictures/book.svg"></router-link>
+      <router-link to="/a" class=""><img src="../assets/pictures/user.svg"></router-link>
+      <router-link to="/b" class=""><img src="../assets/pictures/settings.svg"></router-link>
+    </div>
+    <a id="roles"></a>
   </div>
 </template>
