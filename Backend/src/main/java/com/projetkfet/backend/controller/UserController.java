877f90a25f28b2128c8e37711de2d14aa6e25c89
Thibault_Azemar <thibault.azemar@gmail.com>
2023-02-14 13:35:36 +0100

    Delete and Update

--- Backend/src/main/java/com/projetkfet/backend/controller/UserController.java
+++ Backend/src/main/java/com/projetkfet/backend/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 
 // Classe contrôlleur des requêtes pour la classe User
 @Controller
@@ -14,11 +16,31 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+//  Get
+
+//    Retourne l'id de l'utilisateur avec lequel on se connecte
+    @GetMapping(path="/connect")
+    public @ResponseBody
+    Integer connectUser (@RequestParam("name") String name, @RequestParam("password") String password)
+    {
+        return userRepository.findByNameAndPassword(name, password).getId();
+    }
+
+//    Retourne la liste de tous les utilisateurs
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers()
+    {
+        return userRepository.findAll();
+    }
+
+//  Post
+
+//    Permet d'ajouter un nouvel utilisateur
     @PostMapping(path="/add")
     public @ResponseBody
-    String addNewUser (@RequestParam String name
-            , @RequestParam String password) {
-
+    String addNewUser (@RequestParam("name") String name, @RequestParam("password") String password)
+    {
+//        TODO: Vérifier que le name n'est pas déjà utilisé
         User n = new User();
         n.setName(name);
         n.setPassword(password);
@@ -26,15 +48,33 @@ public class UserController {
         return "Saved";
     }
 
-    @GetMapping(path="/connect")
-    public @ResponseBody
-    User connectUser (@RequestParam String name
-            , @RequestParam String password) {
-        return userRepository.findByNameAndPassword(name, password);
+//  Update
+    @PatchMapping()
+    public @ResponseBody void updateUser(@RequestParam("id") Integer id, @RequestParam(required = false, name = "name") String name, @RequestParam(required = false, name = "password") String password)
+    {
+        Optional<User> n = userRepository.findById(id);
+
+        // if n est non null
+        if (n.isPresent())
+        {
+            User user = n.get();
+
+            if (name != null)
+            {
+                user.setName(name);
+            }
+            if (password != null)
+            {
+                user.setPassword(password);
+            }
+        }
     }
 
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+//    Delete
+    @DeleteMapping()
+    public @ResponseBody void deleteUser(@RequestParam("id") Integer id)
+    {
+        userRepository.deleteById(id);
     }
+
 }
