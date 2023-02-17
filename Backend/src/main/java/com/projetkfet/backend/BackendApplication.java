14634c57654ec583796fc7a49878095f1fa836a6
Thibault_Azemar <thibault.azemar@gmail.com>
2023-02-17 16:42:42 +0100

    Add log4j2 + UserLogger

--- Backend/src/main/java/com/projetkfet/backend/BackendApplication.java
+++ Backend/src/main/java/com/projetkfet/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.projetkfet.backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,12 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("http://localhost:8081/")
 public class BackendApplication {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        logger.info("A Hello World Message");
         return String.format("Hello %s!", name);
     }
 
--- Backend/src/main/java/com/projetkfet/backend/controller/UserController.java
+++ Backend/src/main/java/com/projetkfet/backend/controller/UserController.java
@@ -2,6 +2,8 @@ package com.projetkfet.backend.controller;
 
 import com.projetkfet.backend.data.UserRepository;
 import com.projetkfet.backend.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,9 @@ import java.util.Optional;
 @Controller
 @RequestMapping(path="/user")
 public class UserController {
+
+    Logger logger = LoggerFactory.getLogger("UserLogger");
+
     @Autowired
     private UserRepository userRepository;
 
@@ -22,6 +27,7 @@ public class UserController {
     @GetMapping(path="/all")
     public @ResponseBody Iterable<User> getAllUsers()
     {
+        logger.info("All Users");
         return userRepository.findAll();
     }
 
@@ -32,6 +38,7 @@ public class UserController {
     public @ResponseBody
     Integer connectUser (@RequestParam("name") String name, @RequestParam("password") String password)
     {
+        logger.info("Connect User");
         return userRepository.findByNameAndPassword(name, password).getId();
     }
 
@@ -41,6 +48,8 @@ public class UserController {
     String addNewUser (@RequestParam("name") String name, @RequestParam("password") String password)
     {
 //        TODO: Vérifier que le name n'est pas déjà utilisé
+        logger.info("New User");
+
         User n = new User();
         n.setName(name);
         n.setPassword(password);
@@ -52,6 +61,8 @@ public class UserController {
     @PatchMapping()
     public @ResponseBody void updateUser(@RequestParam("id") Integer id, @RequestParam(required = false, name = "name") String name, @RequestParam(required = false, name = "password") String password)
     {
+        logger.info("Update User");
+
         Optional<User> n = userRepository.findById(id);
 
         // if n est non null
@@ -76,6 +87,7 @@ public class UserController {
     @DeleteMapping()
     public @ResponseBody void deleteUser(@RequestParam("id") Integer id)
     {
+        logger.info("Delete User");
         userRepository.deleteById(id);
     }
 
