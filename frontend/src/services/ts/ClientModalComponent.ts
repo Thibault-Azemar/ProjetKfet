466d9520fe290d97ab2a8b2b12c094ee22167351
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2023-03-17 16:22:59 +0100

    add addaccount

--- frontend/src/services/ts/ClientModalComponent.ts
+++ frontend/src/services/ts/ClientModalComponent.ts
@@ -2,34 +2,53 @@ import ClientModalComponent from '../../components/ClientModalComponent.vue'
 import { defineComponent } from 'vue'
 import '../../assets/style/comptes.css'
 import Customer from "../model/CustomerModel"
+import Group from '../model/GroupModel'
+import AccountsRepository from '../Repository/AccountsRepository'
 
 
 // @ts-ignore
 // @ts-ignore
 export default defineComponent({
 
-    components:{
+    components: {
         ClientModalComponent,
     },
     props: {
-        customer : {
-            type : Customer,
-            require : false
+        customer: {
+            type: Customer,
+            require: false
         }
     },
     data() {
-
+        const groups: Group[] = []
+            ;
+        return {
+            groups
+        }
     },
-    methods:{
+    methods: {
+        getGroups() {
+            const accountsRepo = new AccountsRepository();
+            accountsRepo.getGroups().then((groups: Group[]) => {
+                this.groups = groups;
+                console.log(this.groups)
+            });
+        },
         unshowModal(idModal: string) {
-            this.$emit('unshowModal',idModal);
+            this.$emit('unshowModal', idModal);
         },
-        addCompte(){
-
+        addCompte() {
+            const accountsRepo = new AccountsRepository();
+            accountsRepo.addAccount("test", "test", 100, "DI5").then((customer: Customer) => {
+                //location.reload();
+            });
         },
-        editCompte(){
+        editCompte() {
 
         }
-    }
+    },
+    beforeMount() {
+        this.getGroups();
+    },
 
 })
