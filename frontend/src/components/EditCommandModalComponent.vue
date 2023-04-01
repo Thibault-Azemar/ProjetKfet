046988c9cb2e7afa6fcb4296c0e2c06214f248a9
Aurélie THURIOT <aurelie.thuriot@etu.univ-tours.fr>
2023-04-01 11:55:26 +0200

    commande state

--- frontend/src/components/EditCommandModalComponent.vue
+++ frontend/src/components/EditCommandModalComponent.vue
@@ -11,35 +11,35 @@
         <div>{{commande.name}}</div>
         <div>Heure : {{commande.date.getHours()}}:{{commande.date.getMinutes() < 10 ? '0' + commande.date.getMinutes() : commande.date.getMinutes() }}</div>
         <div>Prix : {{commande.total}}€</div>
-        <div>Status : mettre status</div>
+        <div>Status : <p :class="commande.state">{{ commande.state }}</p></div>
         <br>
         Produits :
         <br>
         <div style="max-height: 300px; overflow-y: auto; overflow-x: hidden">
           <div style="display: grid; grid-template-columns: 120px 120px; align-items: center;" v-for="produit in commande.products" :key="produit.id">
               {{produit.name}} test test test test
-              <select class="input-status" @change="editProductState(produit)">
-                <option value="wait" >En attente</option>
-                <option value="loading">En cours</option>
-                <option value="end">Terminé</option>
+              <select class="input-status" :class="produit.state" @change="editProductState(produit)">
+                <option value="wait" :selected="produit.state === 'wait'">En attente</option>
+                <option value="loading" :selected="produit.state === 'loading'">En cours</option>
+                <option value="end" :selected="produit.state === 'end'">Terminé</option>
               </select>
             {{produit.name}}
-            <select class="input-status" @change="editProductState(produit)">
-              <option value="wait" >En attente</option>
-              <option value="loading">En cours</option>
-              <option value="end">Terminé</option>
+            <select class="input-status" :class="produit.state" @change="editProductState(produit)">
+              <option value="wait" :selected="produit.state === 'wait'" >En attente</option>
+              <option value="loading"  :selected="produit.state === 'loading'">En cours</option>
+              <option value="end"  :selected="produit.state === 'end'">Terminé</option>
             </select>
             {{produit.name}}
-            <select class="input-status" @change="editProductState(produit)">
-              <option value="wait" >En attente</option>
-              <option value="loading">En cours</option>
-              <option value="end">Terminé</option>
+            <select class="input-status" :class="produit.state" @change="editProductState(produit)">
+              <option value="wait" :selected="produit.state === 'wait'" >En attente</option>
+              <option value="loading" :selected="produit.state === 'loading'">En cours</option>
+              <option value="end" :selected="produit.state === 'end'">Terminé</option>
             </select>
             {{produit.name}}
-            <select class="input-status" @change="editProductState(produit)">
-              <option value="wait" >En attente</option>
-              <option value="loading">En cours</option>
-              <option value="end">Terminé</option>
+            <select class="input-status" :class="produit.state" @change="editProductState(produit)">
+              <option class="wait" value="wait" :selected="produit.state === 'wait'" >En attente</option>
+              <option class="loading" value="loading" :selected="produit.state === 'loading'">En cours</option>
+              <option value="end" :selected="produit.state === 'end'">Terminé</option>
             </select>
           </div>
         </div>
